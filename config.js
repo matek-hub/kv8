f61c43e92dc3bc1ab65884b8e6db11ab8473adad
matek-hub <email.to.tibi@gmail.com>
2026-07-29 21:53:17 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // Cseréld ki a saját, deployolt Google Apps Script Web App URL-re
 // (Apps Script szerkesztőben: Telepítés -> Új telepítés -> Webalkalmazás -> URL másolása)
-var APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AZ_ID_IDE/exec';
+var APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzNnGCJg7PdN0T10-o4NvSHLg9ciaeqmRx2neypY5UpkjeWBQyYHKHLfQlkFtuonxgcZA/exec';
 
 // Cseréld ki a saját Google OAuth Client ID-re
 // (Google Cloud Console -> APIs & Services -> Credentials -> OAuth 2.0 Client ID -> Web application)
-var GOOGLE_CLIENT_ID = 'IDE_JON_A_CLIENT_ID.apps.googleusercontent.com';
+var GOOGLE_CLIENT_ID = '299880504760-3665hhiu4r8anq3jco9a015b7vtuj1at.apps.googleusercontent.com';
 
 var TOTAL_TESZT = 40;      // 40 teszt, mindegyikben 12 feladat
