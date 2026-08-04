04aea8917e8615eb1320cfaadfda755b019fd371
matek-hub <email.to.tibi@gmail.com>
2026-08-04 15:12:03 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Cseréld ki a saját, deployolt Google Apps Script Web App URL-re
 // (Apps Script szerkesztőben: Telepítés -> Új telepítés -> Webalkalmazás -> URL másolása)
-var APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwe_9aIriJ5QfKRSES1Soe_hX5J38wsk0cHRvWO85m3a7AkujGR4wxRArvkn3nc5sjHCQ/exec';
+var APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwtNeJec9KySOwpXb2_T74Jw8GysHfRh7Cnc4FbDFLyypX-FDgAe-fH40TShZUWNsaUxA/exec';
 
 // Cseréld ki a saját Google OAuth Client ID-re
 // (Google Cloud Console -> APIs & Services -> Credentials -> OAuth 2.0 Client ID -> Web application)
